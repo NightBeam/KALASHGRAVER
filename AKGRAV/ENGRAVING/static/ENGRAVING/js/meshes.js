47f6b1d4b155d67eff3dfeb7ed1d629d4c70396f
Matvey <id.matrix.2006@gmail.com>
2022-04-21 18:56:20 +0400

    meshes added

--- AKGRAV/ENGRAVING/static/ENGRAVING/js/meshes.js
+++ AKGRAV/ENGRAVING/static/ENGRAVING/js/meshes.js
@@ -1,3 +1,27 @@
-canvas = document.querySelector('#canvasForModels');
-width = window.innerWidth;
-height = window.innerHeight;
+'use strict'
+
+var canvas = document.getElementById('canvasForModels');
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+canvas.setAttribute('width', width);
+canvas.setAttribute('height', height);
+ 
+var renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setClearColor(0x000000);
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
+camera.position.set(0,0,1000);
+
+
+var light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+var geometry = new THREE.SphereGeometry(200, 12, 12);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+var mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+renderer.render(scene, camera);
